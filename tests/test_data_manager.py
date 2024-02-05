7ad1394ba83b2ef31af1e65e977f40e1b96d0cbc
miriam-horovicz-ni <136950252+miriam-horovicz-ni@users.noreply.github.com>
2024-02-05 16:39:24 +0200

    Local Path

--- tests/test_data_manager.py
+++ tests/test_data_manager.py
@@ -14,12 +14,15 @@ ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if ROOT_PATH not in sys.path:
     sys.path.append(ROOT_PATH)
 
+
+
 from simpml.core.base import Dataset, PredictionType
 from simpml.tabular.adapters_pool import ManipulateAdapter
 from simpml.tabular.splitter_pool import RandomSplitter
 from simpml.tabular.tabular_data_manager import SupervisedTabularDataManager
 
-TEST_DATA_DIR: str = os.path.join(ROOT_PATH, "tests", "data")
+sys.path.insert(0, '.') 
+TEST_DATA_DIR: str = os.path.join("data")
 
 
 def test_get_data() -> None:
