4e37ee5bccade0c55f2f9558c47a73300ae5c8bf
miriam-horovicz-ni <136950252+miriam-horovicz-ni@users.noreply.github.com>
2024-02-05 19:01:14 +0200

    add root path

--- tests/test_data_fetcher.py
+++ tests/test_data_fetcher.py
@@ -3,9 +3,12 @@
 from __future__ import annotations
 
 import os
+import sys
 from typing import Any, Dict
 
-from simpml.tabular.data_fetcher_pool import TabularDataFetcher
+ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
 
 TEST_DATA_DIR: str = os.path.join("tests", "data")
 
