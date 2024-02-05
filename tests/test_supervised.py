24f59486d7a9fca2c4e7956f66276856c68483d6
miriam-horovicz-ni <136950252+miriam-horovicz-ni@users.noreply.github.com>
2024-02-05 19:08:44 +0200

    fix root path

--- tests/test_supervised.py
+++ tests/test_supervised.py
@@ -3,11 +3,16 @@
 from __future__ import annotations
 
 import os
+import sys
 from typing import cast
 
 import pandas as pd
 import plotly.graph_objects as go
 import pytest
+
+ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
 
 TEST_DATA_DIR: str = os.path.join("tests", "data")
 
