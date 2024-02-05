396de7e89f6facb3e54ceddafc50355ea7da4047
miriam-horovicz-ni <136950252+miriam-horovicz-ni@users.noreply.github.com>
2024-02-05 18:12:48 +0200

    Fix test path

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,13 +3,8 @@
 from __future__ import annotations
 
 import os
-import sys
 
 import pytest
-
-ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if ROOT_PATH not in sys.path:
-    sys.path.append(ROOT_PATH)
 
 from simpml.core.base import (
     Dataset,
@@ -22,7 +17,7 @@ from simpml.core.base import (
     VariableType,
 )
 
-TEST_DATA_DIR: str = os.path.join(ROOT_PATH, "tests", "data")
+TEST_DATA_DIR: str = os.path.join("tests", "data")
 
 
 def test_enums() -> None:
