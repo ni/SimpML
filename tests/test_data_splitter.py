9738de1bc29eb77d0b98041fbf5f2aedd0b4cd48
miriam-horovicz-ni <136950252+miriam-horovicz-ni@users.noreply.github.com>
2024-02-05 18:14:16 +0200

    Fix data path

--- tests/test_data_splitter.py
+++ tests/test_data_splitter.py
@@ -4,12 +4,7 @@
 from __future__ import annotations
 
 import os
-import sys
 from typing import Any, Dict
-
-ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if ROOT_PATH not in sys.path:
-    sys.path.append(ROOT_PATH)
 
 from simpml.core.base import Dataset
 from simpml.tabular.data_fetcher_pool import TabularDataFetcher
@@ -20,7 +15,7 @@ from simpml.tabular.splitter_pool import (
     RandomSplitter,
 )
 
-TEST_DATA_DIR: str = os.path.join(ROOT_PATH, "tests", "data")
+TEST_DATA_DIR: str = os.path.join("tests", "data")
 
 
 def test_random_splitter() -> None:
