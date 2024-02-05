50d5fe62c829b2fecb9815e3cadc1037dd1683c8
miriam-horovicz-ni <136950252+miriam-horovicz-ni@users.noreply.github.com>
2024-02-05 19:10:28 +0200

    fix root path

--- tests/test_vision.py
+++ tests/test_vision.py
@@ -3,9 +3,14 @@
 from __future__ import annotations
 
 import os
+import sys
 
 import pytest
 from fastai.vision.all import get_image_files, ImageDataLoaders, Resize, untar_data, URLs
+
+ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
 
 TEST_DATA_DIR: str = os.path.join("tests", "data")
 
