d85d52116982925e117395585958a06f7b696634
miriam-horovicz-ni <136950252+miriam-horovicz-ni@users.noreply.github.com>
2024-02-05 18:17:06 +0200

    Fix tests path

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -3,15 +3,10 @@
 from __future__ import annotations
 
 import os
-import sys
-
-ROOT_PATH: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-if ROOT_PATH not in sys.path:
-    sys.path.append(ROOT_PATH)
 
 from simpml.tabular.steps_pool import SmartImpute
 
-TEST_DATA_DIR: str = os.path.join(ROOT_PATH, "tests", "data")
+TEST_DATA_DIR: str = os.path.join("tests", "data")
 
 
 def test_smart_impute() -> None:
